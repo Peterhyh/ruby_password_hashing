d9952e7f07a1ab05de9a0f26613345293ad075c0
Peter Huynh <peterhyh@yahoo.com>
2024-12-30 16:16:13 -0600

    list of users added

--- password_hashing.rb
+++ password_hashing.rb
@@ -1,5 +1,15 @@
 require 'bcrypt'
 include BCrypt
 
+#List of user's usernames and passwords
+
+users = [
+  {username: "bob", password: "bobpassword"},
+  {username: "john", password: "johnpassword"},
+  {username: "terry", password: "terrypassword"},
+  {username: "peter", password: "peterpassword"},
+]
+
+
 my_password = BCrypt::Password.create("my password")
 puts my_password == "my_password"
