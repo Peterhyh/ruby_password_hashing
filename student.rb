4da9af35f3d680805e8bef1f9789331411826763
Peter Huynh <peterhyh@yahoo.com>
2025-01-16 20:10:28 -0600

    student class file  added

---
+++ student.rb
@@ -0,0 +1,12 @@
+class Student
+  attr_accessor  :first_name, :last_name, :email, :username, :password
+
+  def initialize(first_name, last_name, email, username, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+  end
+
+end
